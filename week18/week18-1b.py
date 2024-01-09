8aeba04e89846e478b1066f920edf8f9e7f82a51
LinPP0000 <linp55664@gmail.com>
2024-01-09 11:29:52 +0800

    week18

---
+++ week18/week18-1b.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=(a-1500)//250
+if b%250>0:
+	print(100+b*5+5,end='')
+else:
+	print(100+b*5,end='') 
+       
+ 
+         
---
+++ week18/week18-3b.py
@@ -0,0 +1,4 @@
+a = input('輸入數字:')
+b = input('在輸入數字:')
+ans = int(a) + int(b)
+print('答案是', ans)
